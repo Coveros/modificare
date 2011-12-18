42aa54df0c62c230f4211e5f2dcbc9806f5ffde0
Jonathan Miller Kauffman <kauffmj@gmail.com>
2011-12-18 08:25:05 -0500

    I have fixed an error with the NAPFD calculator that caused it to throw an error whenever a reduced test suite contains one test case.

--- src/Fitness/FDP_Fit.R
+++ src/Fitness/FDP_Fit.R
@@ -11,8 +11,31 @@ APFD <- function(Ord, lFM)
 {
   #Number of Tests
   FbyT <- dim(lFM)
-  
-  reveal <- apply(lFM[,Ord], 1, function(x) {which(x)[1]})
+
+  # Handle the case when there is only one test case in a reduction.
+  if(length(Ord) == 1)
+  {
+    # Since there is only one test case, it is the first test case to reveal
+    # all the faults that it reveals.
+    reveal <- lFM[,Ord]
+    reveal <- as.numeric(reveal)
+    # Change all 0 values to NA.
+    # NOTE: The original author of this function claims that for loops are less
+    # efficient than apply, but this article suggests otherwise:
+    # http://stackoverflow.com/questions/1504832/help-me-replace-a-for-loop-with-an-apply-function
+    for(x in 1:length(reveal))
+    {
+        if(reveal[x] == 0)
+        {
+            reveal[x] = NA
+        }
+    }
+  }
+  else
+  {
+    reveal <- apply(lFM[,Ord], 1, function(x) {which(x)[1]})
+  }
+
   # Removes NA values before computing reveal.
   # reveal <- apply(lFM[,Ord][ , !apply(is.na(lFM[,Ord]), 2, all)], 1,
   # function(x) {which(x)[1]})
